fbbb8b727ae83e69b3e377c57bc070f9c14f9511
Diogo Madeira <diogomadeira10@gmail.com>
2023-11-10 13:15:35 +0000

    recipe hover effect

--- template-parts/receitas/content-single.php
+++ template-parts/receitas/content-single.php
@@ -2,11 +2,11 @@
 
     <div class="flex gap-10">
 
-        <div class="flex-1">
+        <div class="flex-1 overflow-hidden">
             <?php if (has_post_thumbnail()) : ?>
-                <a href="<?php the_permalink(); ?>" alt="<?php the_title_attribute(); ?>">
-                    <?php the_post_thumbnail(); ?>
-                </a>
+                <?php
+                echo $post_thumbnail = get_the_post_thumbnail(null, 'post-thumbnail', array('class' => 'hover:scale-125 transition duration-500 cursor-pointer object-cover'));
+                ?>
             <?php else : ?>
                 <img class="object-cover max-h-96 w-full border" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/img-placeholder.png'); ?>" alt="img-placeholder">
             <?php endif; ?>
@@ -43,14 +43,26 @@
                         ?>
                     </div>
                 </div>
-                <div>
+                <div class="flex gap-2">
                     <?php
                     foreach ((get_the_category()) as $category) {
                     ?>
-                        <span class="bg-primary/10 text-primary text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full"><?php echo $category->name ?></span>
+                        <span class="border-2 border-primary/20 text-dark text-xs font-normal w-auto h-6 flex px-2.5 py-0.5 items-center justify-center rounded-full"><?php echo $category->name ?></span>
                     <?php
                     }
                     ?>
+                    <div class="border-2 border-primary/20 text-dark text-xs font-normal w-6 h-6 flex items-center justify-center rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>
+                    </div>
+                    <div class="border-2 border-primary/20 text-dark text-xs font-normal w-6 h-6 flex items-center justify-center rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-instagram"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>                   
+                    </div>
+                    <div class="border-2 border-primary/20 text-dark text-xs font-normal w-6 h-6 flex items-center justify-center rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-twitter"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"/></svg>
+                    </div>
+                    <div class="border-2 border-primary/20 text-dark text-xs font-normal w-6 h-6 flex items-center justify-center rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>                    
+                    </div>
                 </div>
             </header>
 
