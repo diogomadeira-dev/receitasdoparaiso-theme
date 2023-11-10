d3e2f106e5802b3efd5ba3a87b0b5af2f7cd290b
Diogo Madeira <diogomadeira10@gmail.com>
2023-11-10 21:56:22 +0000

    Update content-single.php

--- template-parts/receitas/content-single.php
+++ template-parts/receitas/content-single.php
@@ -144,9 +144,12 @@
 
                 <?php $url = get_post_meta(get_the_ID(), 'url', true); ?>
 
-                <?php if (!empty($url)) { 
-                    echo '<a role="button" class="btn btn-primary" href="' . esc_url($url) . '" target="_blank">' . "Ver vídeo" . '</a>';
-                } ?>
+                <?php if (!empty($url)) { ?>
+                    <a role="button" class="btn btn-primary" href="<?php echo esc_url($url); ?>" target="_blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-youtube"><path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17"/><path d="m10 15 5-3-5-3z"/></svg>
+                    Ver vídeo
+                </a>
+                <?php } ?>
                 
                 <?php
                 // wp_link_pages(
