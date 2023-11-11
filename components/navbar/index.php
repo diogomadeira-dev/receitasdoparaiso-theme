1805737ffe216430bd37b30cb92bce213f085b9f
Diogo Madeira <diogomadeira10@gmail.com>
2023-11-11 20:40:26 +0000

    add product quantity no cart icon

--- components/navbar/index.php
+++ components/navbar/index.php
@@ -8,6 +8,7 @@
         <li><a href="/">Início</a></li>
         <li><a href="/receitas">Receitas</a></li>
         <li><a href="/loja">Loja</a></li>
+        <li><a href="/sobre">Sobre nós</a></li>
         <!-- <li>
           <a>Parent</a>
           <ul class="p-2">
@@ -18,7 +19,7 @@
       </ul>
     </div>
 
-    <div class="w-40 h-20">
+    <div class="flex items-center w-40 h-20">
         <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/svg/logo-horizontal-light.svg'); ?>" alt="Logótipo Receitas do Paraíso" class="w-auto h-auto">
     </div>
 
@@ -28,6 +29,7 @@
       <li><a href="/">Início</a></li>
       <li><a href="/receitas">Receitas</a></li>
       <li><a href="/loja">Loja</a></li>
+      <li><a href="/sobre">Sobre nós</a></li>
       <!-- <li tabindex="0">
         <details>
           <summary>Parent</summary>
@@ -45,12 +47,12 @@
         <label tabindex="0" class="btn btn-ghost btn-circle">
             <div class="indicator">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-bag"><path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z"/><path d="M3 6h18"/><path d="M16 10a4 4 0 0 1-8 0"/></svg>           
-                <span class="badge-cart badge-sm indicator-item text-xxs">99</span>
+                <span class="badge-cart badge-sm indicator-item text-xxs"><?php echo WC()->cart->get_cart_contents_count() ?></span>
             </div>
         </label>
         <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
             <div class="card-body">
-            <span class="font-bold text-lg">8 Items</span>
+            <span class="font-bold text-lg"><?php echo WC()->cart->get_cart_contents_count() ?> produtos</span>
             <span class="text-info">Subtotal: $999</span>
             <div class="card-actions">
                 <button class="btn btn-primary btn-block">View cart</button>
