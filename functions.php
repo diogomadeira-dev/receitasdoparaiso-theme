315d32482a6f2962d2eacc14f3c5098f22b7bb4a
Diogo Madeira <diogomadeira10@gmail.com>
2023-11-12 23:05:22 +0000

    add favicon

--- functions.php
+++ functions.php
@@ -203,7 +203,9 @@ add_action('admin_head', 'RemoveAddMediaButtonsForNonAdmins');
 
 require 'inc/recipes/recipes-functions.php';
 
-// Activate WordPress Maintenance Mode	
+/**
+* Activate WordPress Maintenance Mode	
+*/
 function wp_maintenance_mode() {
 	if (!current_user_can('edit_themes') || !is_user_logged_in()) {
 		wp_die('<h1>Em desenvolvimento</h1>');
@@ -211,6 +213,22 @@ function wp_maintenance_mode() {
 }
 
 add_action('get_header', 'wp_maintenance_mode');
+
+/**
+* Favicon
+*/
+
+function add_my_favicon() {
+	$favicon_path = get_template_directory_uri() . '/assets/favicon/';
+	
+ 	echo '<link rel="apple-touch-icon" sizes="180x180" href="' . esc_url($favicon_path) . 'apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32"  href="' . esc_url($favicon_path) . 'favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="' . esc_url($favicon_path) . 'favicon-16x16.png" />
+	<link rel="manifest" href="' . esc_url($favicon_path) . 'site.webmanifest" />';
+ }
+ 
+ add_action( 'wp_head', 'add_my_favicon' ); //front end
+ add_action( 'admin_head', 'add_my_favicon' ); //admin end
 
 // function set_static_front_page() {
 //     // Check if the front page is already set
