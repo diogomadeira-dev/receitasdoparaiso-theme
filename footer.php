99a20f41fbb2e087bdf09fba351ff640489cefeb
Diogo Madeira <diogomadeira10@gmail.com>
2023-11-17 23:39:18 +0000

    fix footer

--- footer.php
+++ footer.php
@@ -7,166 +7,129 @@
 
 <?php do_action( 'receitasdoparaiso_theme_content_after' ); ?>
 
-<!-- <footer id="colophon" class="site-footer bg-gray-50 py-12" role="contentinfo">
-	<?php do_action( 'receitasdoparaiso_theme_footer' ); ?>
 
-	<div class="text-center text-gray-500">
-		&copy; <?php echo date_i18n( 'Y' );?> - <?php echo get_bloginfo( 'name' );?>
-	</div>
-</footer> -->
-
-
-<div class="relative mt-16 text-neutral-500 bg-neutral-900">
+<footer class="relative mt-16 text-neutral-500 bg-neutral-950">
   <svg class="absolute top-0 w-full h-6 -mt-5 sm:-mt-10 sm:h-16 text-deep-purple-accent-400" preserveAspectRatio="none" viewBox="0 0 1440 54">
-    <path class="fill-neutral-900" d="M0 22L120 16.7C240 11 480 1.00001 720 0.700012C960 1.00001 1200 11 1320 16.7L1440 22V54H1320C1200 54 960 54 720 54C480 54 240 54 120 54H0V22Z"></path>
+    <path class="fill-neutral-950" d="M0 22L120 16.7C240 11 480 1.00001 720 0.700012C960 1.00001 1200 11 1320 16.7L1440 22V54H1320C1200 54 960 54 720 54C480 54 240 54 120 54H0V22Z"></path>
   </svg>
   <div class="px-4 pt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
     <div class="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
       <div class="md:max-w-md lg:col-span-2">
-        <a href="/" aria-label="Go home" title="Company" class="inline-flex items-center w-full">
-          <!-- <svg class="w-8 text-teal-accent-400" viewBox="0 0 24 24" stroke-linejoin="round" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" stroke="currentColor" fill="none">
-            <rect x="3" y="1" width="7" height="12"></rect>
-            <rect x="3" y="17" width="7" height="6"></rect>
-            <rect x="14" y="1" width="7" height="6"></rect>
-            <rect x="14" y="11" width="7" height="12"></rect>
-          </svg>
-          <span class="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">Company</span> -->
-		  <div class="flex justify-center items-start w-full h-20">
-        	  <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/svg/logo-horizontal-light.svg'); ?>" alt="Logótipo Receitas do Paraíso" class="w-auto h-full">
+		    <div class="flex justify-center items-start w-full h-16">
+        	  <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/svg/logo-horizontal-dark.svg'); ?>" alt="Logótipo Receitas do Paraíso" class="w-auto h-full">
 	      </div>
-     </a>
-        <!-- <div class="mt-4 lg:max-w-sm">
-          <p class="text-sm text-deep-purple-50">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
-          </p>
-          <p class="mt-4 text-sm text-deep-purple-50">
-            Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-          </p>
-        </div> -->
       </div>
-      <div class="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
+      <div class="grid grid-cols-3 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-3 text-sm">
         <div>
-          <p class="font-semibold tracking-wide text-primary">
-            Category
-          </p>
           <ul class="mt-2 space-y-2">
             <li>
-              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">News</a>
+              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-primary">Contacta-nos</a>
             </li>
             <li>
-              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">World</a>
+              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-primary">Loja</a>
+            </li>
+            <!-- <li>
+              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-primary">Business</a>
             </li>
             <li>
-              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">Games</a>
-            </li>
-            <li>
-              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">References</a>
-            </li>
+              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-primary">Entertainment</a>
+            </li> -->
           </ul>
         </div>
         <div>
-          <p class="font-semibold tracking-wide text-primary">Cherry</p>
-          <ul class="mt-2 space-y-2">
-            <li>
-              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">Contacta-nos</a>
-            </li>
-            <li>
-              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">Loja</a>
-            </li>
-            <li>
-              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">Business</a>
-            </li>
-            <li>
-              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">Entertainment</a>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <p class="font-semibold tracking-wide text-primary">Apples</p>
           <ul class="mt-2 space-y-2">
             <li>
               <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-primary">Equipa</a>
             </li>
             <li>
-              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">Sugestão da Semana</a>
+              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-primary">Sugestão Semanal</a>
             </li>
             <li>
-              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">Sobre Nós</a>
+              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-primary">Sobre Nós</a>
             </li>
             <li>
-              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">Canal do Youtube</a>
+              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-primary">Canal do Youtube</a>
             </li>
           </ul>
         </div>
         <div>
-          <p class="font-semibold tracking-wide text-primary">
-            Business
-          </p>
           <ul class="mt-2 space-y-2">
             <li>
-              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">Condições Gerais de Venda</a>
+              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-primary">Condições Gerais de Venda</a>
             </li>
             <li>
-              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">Resolução Alternativa de Litígios de Consumo</a>
+              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-primary">Resolução Alternativa de Litígios de Consumo</a>
             </li>
             <li>
-              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">Política de Privacidade</a>
+              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-primary">Política de Privacidade</a>
             </li>
             <li>
-              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400">Política de Cookies</a>
+              <a href="/" class="transition-colors duration-300 text-deep-purple-50 hover:text-primary">Política de Cookies</a>
             </li>
           </ul>
         </div>
       </div>
     </div>
-    <div class="flex flex-col justify-between pt-5 pb-10 border-t border-deep-purple-accent-200 sm:flex-row">
-      <p class="text-sm text-gray-100">
+    <div class="flex flex-col justify-between pt-5 pb-10 sm:flex-row">
+      <p class="text-xs text-center sm:text-left">
 	  		&copy; <?php echo date_i18n( 'Y' );?> <?php echo get_bloginfo( 'name' );?>
       </p>
-      <div class="flex items-center mt-4 space-x-4 sm:mt-0">
-        <a href="/" class="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400">
-          <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
+      <div class="flex items-center mt-4 space-x-4 sm:mt-0 justify-center sm:justify-end">
+        <a href="https://www.youtube.com/@ReceitasdoParaiso" target="_blank" class="transition-colors duration-300 text-deep-purple-100 hover:text-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="currentColor"
+            viewBox="0 0 24 24">
             <path
-              d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z"
-            ></path>
+              d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
           </svg>
         </a>
-        <a href="/" class="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400">
-          <svg viewBox="0 0 30 30" fill="currentColor" class="h-6">
-            <circle cx="15" cy="15" r="4"></circle>
+        <a href="https://twitter.com/receitasparaiso" target="_blank" class="transition-colors duration-300 text-deep-purple-100 hover:text-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="currentColor"
+            viewBox="0 0 24 24">
             <path
-              d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z"
-            ></path>
+              d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
           </svg>
         </a>
-        <a href="/" class="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400">
-          <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
+        <a href="https://www.tiktok.com/@receitasdoparaiso" target="_blank" class="transition-colors duration-300 text-deep-purple-100 hover:text-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            class="h-5 w-5">
+            <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
             <path
-              d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z"
-            ></path>
+              fill="currentColor"
+              d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z" />
+          </svg>
+        </a>
+        <a href="https://www.instagram.com/receitasdoparaiso" target="_blank" class="transition-colors duration-300 text-deep-purple-100 hover:text-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="currentColor"
+            viewBox="0 0 24 24">
+            <path
+              d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+          </svg>
+        </a>
+        <a href="https://www.facebook.com/receitasdoparaiso" target="_blank" class="transition-colors duration-300 text-deep-purple-100 hover:text-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="currentColor"
+            viewBox="0 0 24 24">
+            <path
+              d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
           </svg>
         </a>
       </div>
     </div>
   </div>
-</div>
-
-<!-- <footer class="footer footer-center p-10 bg-primary text-primary-content">
-  <aside>
-    <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" class="inline-block fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-    <p class="font-bold">
-      ACME Industries Ltd. <br/>Providing reliable tech since 1992
-    </p> 
-    <p>Copyright © 2023 - All right reserved</p>
-  </aside> 
-  <nav>
-    <div class="grid grid-flow-col gap-4">
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
-    </div>
-  </nav>
-</footer> -->
+</footer>
 
 </div>
 
