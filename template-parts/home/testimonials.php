da49447ddb535bfeb032414fafb48c5cac81fad4
Diogo Madeira <diogomadeira10@gmail.com>
2023-12-05 12:06:19 +0000

    Update testimonials.php

--- template-parts/home/testimonials.php
+++ template-parts/home/testimonials.php
@@ -6,14 +6,13 @@
       <div class="grid gap-x-6 md:grid-cols-3 lg:gap-x-12">
         <div class="mb-12 md:mb-0 card p-6 bg-base-100 shadow-xl">
           <p class="mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos
-            id officiis hic tenetur quae quaerat ad velit ab hic.
+          A variedade, a qualidade, a apresentação e a simpatia da Dona Fernanda, tornaram-me fã das “Receitas do Paraíso”.
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="inline-block w-6">
               <path fill="currentColor"
                 d="M580 556h160V396H580v160Zm-360 0h160V396H220v160Zm406 220 80-160H520V336h280v288l-76 152h-98Zm-360 0 80-160H160V336h280v288l-76 152h-98Zm34-300Zm360 0Z" />
             </svg>
           </p>
-          <h5 class="mb-2 text-sm font-bold">Maria Smantha</h5>
+          <h5 class="mb-2 text-sm font-bold">Rogério Duarte</h5>
           <ul class="mb-0 flex justify-center">
             <li>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
@@ -42,21 +41,21 @@
             <li>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
                 <path fill="currentColor"
-                  d="m480 757 157 95-42-178 138-120-182-16-71-168v387ZM233 976l65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
+                  d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
               </svg>
             </li>
           </ul>
         </div>
         <div class="mb-12 md:mb-0 card p-6 bg-base-100 shadow-xl">
           <p class="mb-4">
-            Ut enim ad minima veniam, quis nostrum exercitationem ullam
-            corporis suscipit laboriosam, nisi ut aliquid commodi.
+          Parabéns pelo canal, Receitas do Paraíso! Adoro as suas receitas reinventadas. 
+          Trazem sempre uma abordagem única e deliciosa. Continuem com esse ótimo trabalho!
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="inline-block w-6">
               <path fill="currentColor"
                 d="M580 556h160V396H580v160Zm-360 0h160V396H220v160Zm406 220 80-160H520V336h280v288l-76 152h-98Zm-360 0 80-160H160V336h280v288l-76 152h-98Zm34-300Zm360 0Z" />
             </svg>
           </p>
-          <h5 class="mb-2 text-sm font-bold">Maria Smantha</h5>
+          <h5 class="mb-2 text-sm font-bold">Isabel Tavares</h5>
           <ul class="mb-0 flex justify-center">
             <li>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
@@ -92,14 +91,14 @@
         </div>
         <div class="mb-0 card p-6 bg-base-100 shadow-xl">
           <p class="mb-4">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui
-            blanditiis praesentium voluptatum deleniti atque corrupti.
+          É incrível como as Receitas do Paraíso conseguem transformar receitas com tanta criatividade! 
+          Estou sempre ansioso por novos vídeos. Vocês realmente elevam o nível de qualquer receita. Parabéns!
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="inline-block w-6">
               <path fill="currentColor"
                 d="M580 556h160V396H580v160Zm-360 0h160V396H220v160Zm406 220 80-160H520V336h280v288l-76 152h-98Zm-360 0 80-160H160V336h280v288l-76 152h-98Zm34-300Zm360 0Z" />
             </svg>
           </p>
-          <h5 class="mb-2 text-sm font-bold">Maria Smantha</h5>
+          <h5 class="mb-2 text-sm font-bold">Tiago Santos</h5>
           <ul class="mb-0 flex justify-center">
             <li>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
@@ -128,7 +127,7 @@
             <li>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="w-5 text-warning">
                 <path fill="currentColor"
-                  d="m323 851 157-94 157 95-42-178 138-120-182-16-71-168-71 167-182 16 138 120-42 178Zm-90 125 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-355Z" />
+                  d="m233 976 65-281L80 506l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z" />
               </svg>
             </li>
           </ul>
