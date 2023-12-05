7e71e4c5750abbadba645fd9bee13397d15fa5dd
Diogo Madeira <diogomadeira10@gmail.com>
2023-12-05 12:23:09 +0000

    fix: recipes categories

--- template-parts/receitas/content-single.php
+++ template-parts/receitas/content-single.php
@@ -45,15 +45,18 @@
                     </div>
                 </div>
                 <div class="flex gap-2">
-                    <?php
-                    $categories = get_the_terms( get_the_ID(), 'categoria' );
 
-                    foreach (($categories) as $category) {
-                    ?>
-                        <span class="badge"><?php echo $category->name ?></span>
-                    <?php
-                    }
-                    ?>
+                    <?php 
+                        $categories = get_the_terms( get_the_ID(), 'categoria' );
+
+                        if ($categories) : 
+                            foreach ($categories as $category) {
+                        ?>
+                            <span class="badge"><?php echo $category->name ?></span>
+                        <?php } ?>
+                    <?php endif; ?>
+
+
                     <div class="badge-social">
                         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>
                     </div>
