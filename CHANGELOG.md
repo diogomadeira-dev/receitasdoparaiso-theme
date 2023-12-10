4c800d7e607b85a4608b384f5d29f4de4966e992
Diogo Madeira <diogomadeira10@gmail.com>
2023-12-10 21:45:29 +0000

    Added christmas recipes section to front page

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # Changelog
 
+## 1.1.2 - 10-12-2023
+
+- Added christmas recipes section to front page.
+
+## 1.1.1 - 10-12-2023
+
+- Category filter moved to the right side.
+
 ## 1.1.0 - 10-12-2023
 
 - Added category filter to recipes list. 
---
+++ template-parts/home/christmas-recipes.php
@@ -0,0 +1,51 @@
+<div class="bg-neutral-50 py-16 sm:py-20">
+  <div class="container mx-auto">
+
+    <h1>Receitas de Natal</h1>
+
+    <?php
+
+    $args = array(  
+      'post_type' => 'receitas',
+      'post_status' => 'publish',
+      'posts_per_page' => 8, 
+      'orderby' => 'date', 
+      'order' => 'DESC', 
+      'tax_query' => array(
+        array(
+            'taxonomy' => 'categoria',
+            'field' => 'slug',
+            'terms'    => 'natal',
+          )
+      )
+    );
+
+    $loop = new WP_Query( $args ); 
+    
+      if ( $loop->have_posts() ) : ?>
+
+        <div class="grid grid-flow-row gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      
+          <?php while ( $loop->have_posts() ) : $loop->the_post(); 
+
+            get_template_part( 'template-parts/receitas', get_post_format() );
+
+          endwhile;
+
+          wp_reset_postdata(); 
+
+          ?>
+
+        </div>
+
+      <?php endif; ?>
+
+      <div class="w-full flex justify-center">
+        <a role="button" class="btn btn-primary btn-md font-light" href="/categoria/natal">
+          Ver mais
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg>
+        </a>
+      </div>
+
+  </div>
+</div>
