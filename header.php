8c0ac1555043fd30168c8390f137add7a5b058d7
Diogo Madeira <diogomadeira10@gmail.com>
2023-12-11 22:52:32 +0000

    quick fix

--- header.php
+++ header.php
@@ -2,8 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<!-- <meta name="viewport" content="width=device-width"> -->
-	<meta name="viewport" content="viewport-fit=cover">
+	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
